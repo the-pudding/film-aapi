a8d8a5e53b3abebebd9657e52fed53378b0b3d25
dorslu <100967102+dorslu@users.noreply.github.com>
2025-01-13 00:34:24 -0800

    Update AAPI.Text.svelte

--- src/components/AAPI/AAPI.Text.svelte
+++ src/components/AAPI/AAPI.Text.svelte
@@ -2,12 +2,27 @@
     export let texts;
 </script>
 
-<div class="textContainer">
+<div class="wrapper">
+  <div class="textContainer">
     {#each texts.split(/\r?\n+/) as text, i}
     <p>{text}</p>
     {/each}
+  </div>
 </div>
 
 <style>
-  
+  /* Centering the wrapper in the middle of the page */
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* Full viewport height */
+    margin: 0;
+  }
+
+  .textContainer {
+    width: 500px;
+    overflow-wrap: break-word;
+    text-align: justify; /* Optional: To center the text inside */
+  }
 </style>
