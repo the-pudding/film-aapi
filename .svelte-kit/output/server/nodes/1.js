2217ec48bd4176ec5b566f84b088b2f7fa07edc7
alvinschang <alvinschang@gmail.com>
2025-05-15 09:18:16 -0400

    update github pages

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CljDbxcG.js","_app/immutable/chunks/scheduler.DCUwlaBk.js","_app/immutable/chunks/index.95tfB9Vq.js","_app/immutable/chunks/entry.-mHtxqzR.js","_app/immutable/chunks/index.B4bnyJm2.js","_app/immutable/chunks/paths.BuSRp3sq.js"];
+export const imports = ["_app/immutable/nodes/1.DSbDLF6c.js","_app/immutable/chunks/scheduler.DCUwlaBk.js","_app/immutable/chunks/index.95tfB9Vq.js","_app/immutable/chunks/entry.CkjfqU_H.js","_app/immutable/chunks/index.B4bnyJm2.js","_app/immutable/chunks/paths.CikrFIDg.js"];
 export const stylesheets = [];
 export const fonts = [];
---
+++ docs/_app/immutable/entry/start.BnosTq-L.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CkjfqU_H.js";export{t as start};
